0deae24cc20658a996232af8465f744bcccd6520
dangkhoauit184 <phamdangkhoa18042006@gmail.com>
2025-03-15 21:27:04 +0700

    Create TuongJ.h

---
+++ CoTuong/TuongJ.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "QuanCo.h"
+
+class TuongJ : public QuanCo {
+public:
+    TuongJ();
+    ~TuongJ();
+
+    void nhap();
+    void xuat();
+    void cachDi();
+};
+#pragma once
