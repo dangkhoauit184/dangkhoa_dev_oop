b62871067f19dd6e01543ed8d68468cf43fc2787
dangkhoauit184 <phamdangkhoa18042006@gmail.com>
2025-03-15 21:23:51 +0700

    Create CoTuong.h

---
+++ CoTuong/CoTuong.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class QuanCo {
+protected:
+    string ten;  
+    string mau;  
+public:
+    QuanCo();
+    ~QuanCo();
+
+    void nhap();
+    void xuat();
+};
+#pragma once
